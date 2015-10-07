a485a47137892115567bdf1e87d907af6f805e57
markjanzer <mark.janzer@gmail.com>
2015-10-06 17:31:21 -0700

    Add acct_groups and copperhead_array

---
+++ week-5/acct_groups.rb
@@ -0,0 +1,22 @@
+ copperheads = [
+"Joshua Abrams", "Syema Ailia", "Kris Bies", "Alexander Blair",
+"Andrew Blum", "Jacob Boer", "Steven Broderick", "Ovi Calvo",
+"Danielle Cameron", "Eran Chazan", "Jonathan Chen", "Un CHoi",
+"Kevin Corso", "Eric Dell'Aringa", "Eunice Do", "Ronny Ewanek",
+"John Paul Chaufan Field", "Eric Freeburg", "Jeffery George",
+"Jamar Gibss", "Paul Gaston Gouron", "Gabrielle Gustilo",
+"Marie-France Han", "Noah Heinrich", "Jack Huang", "Max Iniguez",
+"Mark Janzer", "Michael Jasinski", "Lars Johnson", "Joshua Kim",
+"James Kirkpatrick", "Christopher Lee", "Isaac Lee",
+"Joseph Marion", "Kevin Mark", "Bernadette Masciocchi",
+"Bryan Munroe", "Becca Nelson", "Van Phan", "John Polhill",
+"Jeremy Powell", "Jessie Richardson", "David Roberts",
+"Armani Saldana", "Chris Savage", "Parminder Singh", "Kyle Smith",
+"Aaron Tsai", "Douglas Tsui", "Deanna Warren", "Peter Wiebe",
+"Daniel Woznicki", "Jay Yee", "Nicole Yee", "Burno Zatta" ]
+
+# The input will be this array of names. The output will be an array that contains multiple arrays, with 4-5 people in each.
+# The minimum viable product is this exactly.
+
+# For more challenge, randomize the method. I want to be able to run the method multiple times and get different groups each time.
+

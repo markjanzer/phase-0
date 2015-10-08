c1495c54bfb053b50dbad2af3b26d9e79852113a
markjanzer <mark.janzer@gmail.com>
2015-10-08 15:22:14 -0700

    Complete 5.2

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -1,4 +1,4 @@
- copperheads = [
+copperheads = [
 "Joshua Abrams", "Syema Ailia", "Kris Bies", "Alexander Blair",
 "Andrew Blum", "Jacob Boer", "Steven Broderick", "Ovi Calvo",
 "Danielle Cameron", "Eran Chazan", "Jonathan Chen", "Un CHoi",
@@ -15,8 +15,35 @@
 "Aaron Tsai", "Douglas Tsui", "Deanna Warren", "Peter Wiebe",
 "Daniel Woznicki", "Jay Yee", "Nicole Yee", "Burno Zatta" ]
 
+def acc_groups(arr)
+  grouped_arr = []
+  while arr.length > 0
+    if (arr.length % 5 == 0) || (arr.length % 5 == 4) || (arr.length % 4 == 0)
+      if arr.length % 5 == 0
+        while arr.length > 0
+          grouped_arr << arr.pop(5)
+        end
+      elsif cohort_size % 4 == 0
+        while arr.length > 0
+          cohort
+        grouped_arr << arr.pop(4)
+      else
+        grouped_arr << arr.pop(5)
+      end
+    else
+      grouped_arr << arr.pop(4)
+    end
+  end
+  return grouped_arr
+end
+
+groups = acc_groups(copperheads)
+groups.each {|a| p a.length }
+
+
+# If the cohort size is divisible by four, divisible by five, or divisible by five with a remainder of four, then we go
+
 # The input will be this array of names. The output will be an array that contains multiple arrays, with 4-5 people in each.
 # The minimum viable product is this exactly.
 
 # For more challenge, randomize the method. I want to be able to run the method multiple times and get different groups each time.
-
